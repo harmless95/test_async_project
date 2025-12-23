ebedca221ba3c6f1fc0b29a04ff81d7b7c0ea4ef
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-23 19:11:56 +0700

    create practice3.py we learned what a coroutine function and coroutine object are.

---
+++ project_asyncio/practice_main/practice3.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def nested():
+    return 42
+
+
+async def main():
+    # Schedule nested() to run soon concurrently
+    task = asyncio.create_task(nested())
+
+    # "task" can now be used to cancel "nested()", or
+    # can simply be awaited to wait until it is complete:
+    await task
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
