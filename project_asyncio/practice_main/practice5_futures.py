98638238c50e77303ec16b148bd2de1fa2ac5b89
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-25 19:06:57 +0700

    practice future. asyncio.gather()

---
+++ project_asyncio/practice_main/practice5_futures.py
@@ -0,0 +1,28 @@
+import asyncio
+import time
+
+
+async def some_python_coroutine(delay):
+    await asyncio.sleep(delay=delay)
+    return "A"
+
+
+async def function_that_returns_a_future_object(delay):
+    await asyncio.sleep(delay=delay)
+    return "B"
+
+
+async def main():
+    print(time.strftime("%X"))
+    await function_that_returns_a_future_object(delay=4)
+    print(time.strftime("%X"))
+    result = await asyncio.gather(
+        some_python_coroutine(delay=3),
+        function_that_returns_a_future_object(delay=2),
+    )
+    print(result)
+    print(time.strftime("%X"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
